8b1d72f1c368482b9c5d625941889ab22a81f396
qatari78 <qatari78@gmail.com>
2026-09-20 17:30:45 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1356,7 +1356,7 @@ function requireDashboardAuth(req, res, next) {
 // cannot set custom Authorization headers for WebSocket connections, so we inject
 // the token into proxied requests at the wrapper level.
 function attachGatewayAuthHeader(req) {
-  if (!req?.headers?.authorization && OPENCLAW_GATEWAY_TOKEN) {
+  if (req && OPENCLAW_GATEWAY_TOKEN) {
     req.headers.authorization = `Bearer ${OPENCLAW_GATEWAY_TOKEN}`;
   }
 }
